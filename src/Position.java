0fb0e0f608127151356efc509983b4d7a4411d9e
4015g13 <4015g13@limon.ece.uprm.edu>
2018-08-29 18:57:56 -0400

    finished getYearlySalary method.

--- src/Position.java
+++ src/Position.java
@@ -67,9 +67,10 @@ public class Position {
 	 */
 	public double getYearlySalary() {
 		
-		//Add Code Here [You have to use If/Else to get graded]
-		
-		return 0; //Temporal Return
+		if(jobType == true)
+			return this.hSalary * 40 *4.5 * 12;
+		else
+			return this.hSalary * 20 * 4.5 * 12;
 		
 	}
 }
